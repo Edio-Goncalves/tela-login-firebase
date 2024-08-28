257b5ba72f32d10b1e4ec7fccc4798a38bd8a594
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-08-28 15:31:35 -0300

    Create login validation

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+let emailField = document.querySelector("#email");
+let passwordField = document.querySelector("#password");
+let recoverPassword = document.querySelector("#recover-password-button");
+let loginButton = document.querySelector("#login-button");
+
+console.log(passwordField);
+
+emailField.addEventListener("input", () => {
+  validateField(emailField);
+});
+passwordField.addEventListener("input", () => {
+  validateField(passwordField);
+});
+
+function validateField() {
+  const emailValid = isEmailValid();
+  recoverPassword.disabled = !emailValid;
+  const passwordValid = isPasswordValid();
+  loginButton.disabled = !passwordValid || !emailValid;
+}
+
+function isEmailValid() {
+  let em = emailField.value;
+  if (!em) {
+    return false;
+  }
+  return validateEmail(em);
+}
+
+function isPasswordValid() {
+  let pass = passwordField.value;
+  if (!pass) {
+    return false;
+  }
+  return true;
+}
+
+function validateEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
