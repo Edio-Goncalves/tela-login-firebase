3c83d26f89b8c5edf6b462f72533bf5f62e7822d
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-08-28 22:23:32 -0300

    Create function to activate error messages

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,20 @@ let recoverPassword = document.querySelector("#recover-password-button");
 let loginButton = document.querySelector("#login-button");
 
 emailField.addEventListener("input", () => {
-  validateField(emailField);
+  onChangeEmail();
 });
 passwordField.addEventListener("input", () => {
-  validateField(passwordField);
+  onChangePassword();
 });
 
-function validateField() {
-  const emailValid = isEmailValid();
-  recoverPassword.disabled = !emailValid;
-  const passwordValid = isPasswordValid();
-  loginButton.disabled = !passwordValid || !emailValid;
+function onChangeEmail() {
+  toogleEmailErrors();
+  toogleButtonsDisable();
+}
+
+function onChangePassword() {
+  toogleButtonsDisable();
+  tooglePassword();
 }
 
 function isEmailValid() {
@@ -23,6 +26,37 @@ function isEmailValid() {
     return false;
   }
   return validateEmail(em);
+}
+
+function toogleEmailErrors() {
+  let email = emailField.value;
+  if (!email) {
+    document.querySelector("#email-required-error").style = "display:block";
+  } else {
+    document.querySelector("#email-required-error").style = "display:none";
+  }
+
+  if (validateEmail(email)) {
+    document.querySelector("#email-invalid-error").style.display = "none";
+  } else {
+    document.querySelector("#email-invalid-error").style.display = "block";
+  }
+}
+
+function tooglePassword() {
+  let password = passwordField.value;
+  if (!password) {
+    document.querySelector("#password-required-error").style.display = "block";
+  } else {
+    document.querySelector("#password-required-error").style.display = "none";
+  }
+}
+
+function toogleButtonsDisable() {
+  const emailValid = isEmailValid();
+  recoverPassword.disabled = !emailValid;
+  const passwordValid = isPasswordValid();
+  loginButton.disabled = !passwordValid || !emailValid;
 }
 
 function isPasswordValid() {
