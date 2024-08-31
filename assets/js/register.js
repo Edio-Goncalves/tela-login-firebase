0aad0656c89d254d9376760b4407ce93aebd8be3
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-08-31 14:37:54 -0300

    Create firebase connection to registered data

--- assets/js/register.js
+++ assets/js/register.js
@@ -77,3 +77,30 @@ function isFormValid() {
 form.loginButton().addEventListener("click", () => {
   window.location.href = "../../index.html";
 });
+
+/* Registro */
+
+form.registerButton().addEventListener("click", () => {
+  showLoading();
+
+  const email = form.email().value;
+  const password = form.password().value;
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      hideLoading();
+      window.location.href = "../../pages/home.html";
+    })
+    .catch((error) => {
+      hideLoading();
+      alert(getErrorMessage(error));
+    });
+});
+
+function getErrorMessage(error) {
+  if (error.code == "auth/email-already-in-use") {
+    return "Email já está em uso.";
+  }
+}
